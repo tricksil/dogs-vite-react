6beb9e4d1d7cbf7a9ad0608556acb0dd345d6dc3
Patrick Silva <patrickeeep12@gmail.com>
2024-04-17 07:53:51 -0300

    feat: add base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/[REPO_NAME]/',
+  base: '/dogs-vite-react/',
   plugins: [react(), svgr()],
 });
